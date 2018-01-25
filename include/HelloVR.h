7b1e9d6201077e6d4161f353f9aa7ca9f4e1b829
Sander Mertens <sander.mertens8@gmail.com>
2018-01-24 22:56:01 -0800

    Hello World VR application

---
+++ include/HelloVR.h
@@ -0,0 +1,31 @@
+/* HelloVR.h
+ *
+ * This is the main package file. Include this file in other projects.
+ * Only modify inside the header-end and body-end sections.
+ */
+
+#ifndef HELLOVR_H
+#define HELLOVR_H
+
+#include <corto/corto.h>
+#include <corto/corto.h>
+#include <include/_project.h>
+#include <tags/c/c.h>
+#include <corto/c/c.h>
+#include <tags/tags.h>
+
+/* $header() */
+/* Definitions that are required by package headers (native types) go here. */
+/* $end */
+
+#include <include/_type.h>
+#include <include/_interface.h>
+#include <include/_load.h>
+#include <include/_api.h>
+
+/* $body() */
+/* Definitions here that need your package headers go here. */
+/* $end */
+
+#endif
+
---
+++ src/HelloVR.c
@@ -0,0 +1,10 @@
+#include <include/HelloVR.h>
+
+int cortomain(int argc, char *argv[]) {
+
+    while (true) {
+        corto_sleep(1, 0);
+    }
+
+    return 0;
+}
